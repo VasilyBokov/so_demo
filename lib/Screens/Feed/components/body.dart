1e19c17c03b13888f115f561e55dd14780fc2477
Simon <galakhov.sa16@physics.msu.ru>
2020-12-29 13:36:09 +0300

    Some feed changes (add bottom icons)

--- lib/Screens/Feed/components/body.dart
+++ lib/Screens/Feed/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:so_demo/Screens/EventPage/event_page_f.dart';
 import 'package:so_demo/Screens/Profile/profile_screen.dart';
 import 'package:so_demo/profile.dart';
@@ -10,56 +12,112 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    // для корректного отображения размера на совместимых устройствах
+    WidgetsFlutterBinding.ensureInitialized();
+    ScreenUtil.init(context,
+        designSize: Size(360, 720), allowFontScaling: true);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Center(
-          child: IconButton(
-              iconSize: 30,
-              padding: EdgeInsets.all(10.0),
-              icon: Icon(Icons.picture_in_picture_alt),
-              // при нажатии выводится текст в консоль
-              // и вызывается новый экран Профиля
-              onPressed: () {
-                print("Go to event");
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      // открываем экран профия
-                      builder: (context) => EventPageWithFoto(
-                          // для тестового примера
-                          ),
-                    ));
-              }),
+        SizedBox(
+          height: 100,
         ),
-        Center(
-          child: IconButton(
-              iconSize: 30,
-              padding: EdgeInsets.all(10.0),
-              icon: Icon(Icons.person_add),
-              // при нажатии выводится текст в консоль
-              // и вызывается новый экран Профиля
-              onPressed: () {
-                print("Go profile");
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ProfileScreen(
-                              profile: profiles[0],
-                              myprofile: myprofile,
-                            )));
-                // Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //       // открываем экран профия
-                //       builder: (context) => ProfileScreen(
-                //         // для тестового примера запускаем первый из профилей
-                //         profile: profiles[0],
-                //         myprofile: myprofile,
-                //       ),
-                //     ));
-              }),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: IconButton(
+                  iconSize: 30,
+                  padding: EdgeInsets.all(10.0),
+                  icon: Icon(Icons.picture_in_picture_alt),
+                  // при нажатии выводится текст в консоль
+                  // и вызывается новый экран Профиля
+                  onPressed: () {
+                    print("Go to event");
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          // открываем экран профия
+                          builder: (context) => EventPageWithFoto(
+                              // для тестового примера
+                              ),
+                        ));
+                  }),
+            ),
+            Center(
+              child: IconButton(
+                  iconSize: 30,
+                  padding: EdgeInsets.all(10.0),
+                  icon: Icon(Icons.person_add),
+                  // при нажатии выводится текст в консоль
+                  // и вызывается новый экран Профиля
+                  onPressed: () {
+                    print("Go profile");
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProfileScreen(
+                                  profile: profiles[0],
+                                  myprofile: myprofile,
+                                )));
+                    // Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //       // открываем экран профия
+                    //       builder: (context) => ProfileScreen(
+                    //         // для тестового примера запускаем первый из профилей
+                    //         profile: profiles[0],
+                    //         myprofile: myprofile,
+                    //       ),
+                    //     ));
+                  }),
+            ),
+          ],
         ),
+        Expanded(
+          child: SizedBox(),
+        ),
+        // Контейнер, содержащий все кнопки навигации
+        // выделяется в отдельный виджет
+        Container(
+          // color: Colors.green[50],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              // иконка ленты, главное окно
+              IconButton(
+                iconSize: 25,
+                icon: SvgPicture.asset("assets/icons/Home.svg"),
+                onPressed: () {},
+              ),
+              // Иконка запланированных событий
+              IconButton(
+                iconSize: 25,
+                icon: SvgPicture.asset("assets/icons/Calendar.svg"),
+                onPressed: () {},
+              ),
+              // Иконка добавления события
+              IconButton(
+                iconSize: 35,
+                icon: SvgPicture.asset("assets/icons/Add.svg"),
+                onPressed: () {},
+              ),
+              // Иконка поиска события (как организовать поиск?)
+              IconButton(
+                iconSize: 25,
+                icon: SvgPicture.asset("assets/icons/Message.svg"),
+                onPressed: () {},
+              ),
+              // Иконка моего профиля
+              IconButton(
+                iconSize: 25,
+                icon: SvgPicture.asset("assets/icons/profile.svg"),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
